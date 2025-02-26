6f85b368238d4a31983589a174a76c841cc42baa
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-02-26 11:46:15 +0530

    profile page fixed

--- src/routes/authroute.ts
+++ src/routes/authroute.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { register, verifyAccount,googleAuth, Login } from '../controlles/User/userauth';
 import { userauthmidd } from '../middlware/userauthmidd';
+import uploadImage from '../middlware/uploadimage';
 
 
 
 const router=express.Router();
 
-router.post('/register',register)
+router.post('/register',uploadImage, register)
 router.post('/veryfyacount',userauthmidd,verifyAccount)
 router.post('/googleauth',googleAuth)
 router.post('/Login',Login)
