3b3f8cc99c695e7aea213e98a8f84ce5fc567b79
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2026-01-03 15:12:10 +0530

    email changed and port to 587

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -15,8 +15,8 @@ interface Options {
 const sendEmail = async (options: Options) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
